573f7c2252849addcd591790c883c79725e4e904
NicklasSoderlund <nicklassoderlund96@gmail.com>
2023-02-21 22:13:00 +0100

    fixed api call

--- src/components/ProjectDIsplay/ProjectDisplay.tsx
+++ src/components/ProjectDIsplay/ProjectDisplay.tsx
@@ -1,3 +1,22 @@
+import { Octokit } from "octokit";
+import { useEffect, useState } from "react";
+import { IOctokitResponse } from "../../models/IOctokitResponse";
+import { projectService, repoService } from "../../services/projectService";
+
 export function ProjectDisplay() {
+  const [repo, setRepo] = useState<IOctokitResponse>() 
+ 
+  const repoFetch = async () => {
     
+      let response = await repoService("Finished-Design");
+      console.log(response)
+      setRepo(response)
+
+  }
+  useEffect(() => {repoFetch();}, []);
+  console.log(repo)
+
+  return (
+    <div></div>
+  )
 }
